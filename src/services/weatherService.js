ed1508e442b5835080d96db854a074b2a9128b10
Siddhant Vikas Gaikwad <siddhant_gaikwad2000@outlook.com>
2025-04-25 20:40:29 +0530

    first commit

---
+++ src/services/weatherService.js
@@ -0,0 +1,112 @@
+import { DateTime } from "luxon";
+
+const API_KEY = "92bbf9c84f0bbdb6dff5aab9128b1040";
+const BASE_URL = "https://api.openweathermap.org/data/2.5";
+
+// https://api.openweathermap.org/data/2.5/onecall?lat=48.8534&lon=2.3488&exclude=current,minutely,hourly,alerts&appid=1fa9ff4126d95b8db54f3897a208e91c&units=metric
+
+const getWeatherData = (infoType, searchParams) => {
+  const url = new URL(BASE_URL + "/" + infoType);
+  url.search = new URLSearchParams({ ...searchParams, appid: API_KEY });
+
+  return fetch(url).then((res) => res.json());
+};
+
+const formatCurrentWeather = (data) => {
+  const {
+    coord: { lat, lon },
+    main: { temp, feels_like, temp_min, temp_max, humidity },
+    name,
+    dt,
+    sys: { country, sunrise, sunset },
+    weather,
+    wind: { speed },
+  } = data;
+
+  const { main: details, icon } = weather[0];
+
+  return {
+    lat,
+    lon,
+    temp,
+    feels_like,
+    temp_min,
+    temp_max,
+    humidity,
+    name,
+    dt,
+    country,
+    sunrise,
+    sunset,
+    details,
+    icon,
+    speed,
+  };
+};
+
+const formatForecastWeather = (data) => {
+  let { timezone, daily, hourly } = data;
+  if (daily && daily.length >= 6) {
+    daily = daily.slice(1, 6).map((d) => {
+        return {
+            title: formatToLocalTime(d.dt, timezone, "ccc"),
+            temp: d.temp.day,
+            icon: d.weather[0].icon,
+        };
+    });
+} else {
+    // Handle the case when daily is undefined or has insufficient elements
+    console.error("Invalid or insufficient data in the 'daily' array.");
+    // You might want to set a default value or handle this case in another way
+    daily = [];
+}
+
+
+if (hourly && hourly.length >= 6) {
+  hourly = hourly.slice(1, 6).map((d) => {
+      return {
+          title: formatToLocalTime(d.dt, timezone, "hh:mm a"),
+          temp: d.temp,
+          icon: d.weather[0].icon,
+      };
+  });
+} else {
+  // Handle the case when hourly is undefined or has insufficient elements
+  console.error("Invalid or insufficient data in the 'hourly' array.");
+  // You might want to set a default value or handle this case in another way
+  hourly = [];
+}
+
+  return { timezone, daily, hourly };
+};
+
+const getFormattedWeatherData = async (searchParams) => {
+  const formattedCurrentWeather = await getWeatherData(
+    "weather",
+    searchParams
+  ).then(formatCurrentWeather);
+
+  const { lat, lon } = formattedCurrentWeather;
+
+  const formattedForecastWeather = await getWeatherData("onecall", {
+    lat,
+    lon,
+    exclude: "current,minutely,alerts",
+    units: searchParams.units,
+  }).then(formatForecastWeather);
+
+  return { ...formattedCurrentWeather, ...formattedForecastWeather };
+};
+
+const formatToLocalTime = (
+  secs,
+  zone,
+  format = "cccc, dd LLL yyyy' | Local time: 'hh:mm a"
+) => DateTime.fromSeconds(secs).setZone(zone).toFormat(format);
+
+const iconUrlFromCode = (code) =>
+  `http://openweathermap.org/img/wn/${code}@2x.png`;
+
+export default getFormattedWeatherData;
+
+export { formatToLocalTime, iconUrlFromCode };
